c7741fcd4e6f9de11e76cad26a5798c03489b9c7
Shy F <shyfromdi@gmail.com>
2023-08-18 21:48:11 -0700

    Intro Activity

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-
+    print ('Hello world')
+print ('CS7: Python Programming')
 
 if __name__ == '__main__':
     main()
